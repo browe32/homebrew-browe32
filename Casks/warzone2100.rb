146e98ca53dd1ebba79e1b70e7493eed53d6bd08
Brad Rowe <Brad.Rowe@Pearson.com>
2013-11-18 13:23:31 -0700

    added warzone2100; may the 90s live forever

---
+++ Casks/warzone2100.rb
@@ -0,0 +1,7 @@
+class Warzone2100 < Cask
+  url 'https://sourceforge.net/projects/warzone2100/files/releases/3.1.0/warzone2100-3.1.0_%5Bf21f8a1%5D-lqvideo.dmg/download'
+  homepage 'http://wz2100.net/'
+  version '3.1.0'
+  sha1 'a227badc715403cc227fe82422b00b4c028cded5'
+  link 'Warzone.app'
+end
