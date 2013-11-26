96d151a80b87a11016f37b5d8f1e26e34b22e8d3
Brad Rowe <Brad.Rowe@Pearson.com>
2013-11-26 10:39:42 -0700

    added pcgen from sourceforge

---
+++ Casks/pcgen.rb
@@ -0,0 +1,7 @@
+class Pcgen < Cask
+  url 'http://sourceforge.net/projects/pcgen/files/PCGen%20Stable/6.00.1/pcgen6001_mac_install.dmg/download'
+  homepage 'http://pcgen.sourceforge.net/01_overview.php'
+  version '6.00.1'
+  sha1 '099f256439ab857a1d480f8c0c24548799608a0b'
+  link 'PCGen 6.00.1.app'
+end
