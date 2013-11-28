9d73430fcfb157c10796335f92cc9d9e0f089fb2
Brad Rowe <Brad.Rowe@Pearson.com>
2013-11-28 06:30:49 -0700

    added context free art

---
+++ Casks/contextfree.rb
@@ -0,0 +1,7 @@
+class Contextfree < Cask
+  url 'http://www.contextfreeart.org/download/ContextFree3.0.5.dmg'
+  homepage 'http://www.contextfreeart.org/'
+  version '3.0.5'
+  sha1 'ba2dfecc8ca3d39371f5a6da0196d1417b609939'
+  link 'Context Free.app'
+end
