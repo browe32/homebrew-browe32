3deb48e6b55af638c6ca90e3af52024305f94d91
Brad Rowe <Brad.Rowe@Pearson.com>
2013-11-28 06:27:08 -0700

    added keepassx2 alpha

---
+++ Casks/keepassx2.rb
@@ -0,0 +1,7 @@
+class Keepassx2 < Cask
+  url 'http://www.keepassx.org/dev/attachments/download/38/KeePassX-2.0-alpha4.dmg'
+  homepage 'http://www.keepassx.org'
+  version '2.0-alpha4'
+  md5 '1ed80dc03e4c91d805430af085831283'
+  link 'KeePassX.app'
+end
