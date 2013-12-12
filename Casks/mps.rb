40b764c6f9923fb9d44f950f64adc5d97ff30424
Brad Rowe <Brad.Rowe@Pearson.com>
2013-12-11 19:28:08 -0700

    updated jetbrains mps version

--- Casks/mps.rb
+++ Casks/mps.rb
@@ -1,7 +1,7 @@
 class Mps < Cask
-  url 'http://download.jetbrains.com/mps/30/MPS-3.0.1-macos.dmg'
+  url 'https://download.jetbrains.com/mps/30/MPS-3.0.2-macos.dmg'
   homepage 'http://www.jetbrains.com/mps/'
-  version '3.0.1'
-  md5 '1c7c79bd5ec4d7011acd6c92e9d89969'
+  version '3.0.2'
+  sha256 'cd6459e8408eebf5b295cad5d7b3fa718b3f8f82e4f91a8df0f6fb17182196b2'
   link 'MPS 3.0.app'
 end
