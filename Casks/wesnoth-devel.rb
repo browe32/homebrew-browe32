0bfbdf394064cafc0d2f888fc36e72b11019845f
Brad Rowe <Brad.Rowe@Pearson.com>
2013-12-15 07:59:36 -0700

    added wesnoth - development version

---
+++ Casks/wesnoth-devel.rb
@@ -0,0 +1,7 @@
+class WesnothDevel < Cask
+  url 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.11.7/Wesnoth_1.11.7.dmg/download'
+  homepage 'http://www.wesnoth.org/'
+  version '1.11.7'
+  sha1 '4620f60f6a60ecf598de2d83cc3060ae37c42c90'
+  link 'Wesnoth.app'
+end
