7a91bdf2eb47bb45d7e21b0ae468a14ea98537f6
Brad Rowe <Brad.Rowe@Pearson.com>
2013-12-30 09:41:45 -0700

    warzone 2100 updated to 3.1.1

--- Casks/warzone2100.rb
+++ Casks/warzone2100.rb
@@ -1,7 +1,7 @@
 class Warzone2100 < Cask
-  url 'https://sourceforge.net/projects/warzone2100/files/releases/3.1.0/warzone2100-3.1.0_%5Bf21f8a1%5D-lqvideo.dmg/download'
+  url 'https://sourceforge.net/projects/warzone2100/files/releases/3.1.1/warzone2100-3.1.1-novideo.dmg/download'
   homepage 'http://wz2100.net/'
-  version '3.1.0'
-  sha1 'a227badc715403cc227fe82422b00b4c028cded5'
+  version '3.1.1'
+  sha1 '913936ffb7499da8565cc1fff2c0ca378d929ad6'
   link 'Warzone.app'
 end
