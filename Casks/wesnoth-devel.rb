3b99a0ed4400bd11284cf26436e77865b497f7e8
Bradley Rowe <bradley.rowe1@gmail.com>
2014-01-06 05:38:38 -0700

    updated beta wesnoth

--- Casks/wesnoth-devel.rb
+++ Casks/wesnoth-devel.rb
@@ -1,7 +1,7 @@
 class WesnothDevel < Cask
-  url 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.11.7/Wesnoth_1.11.7.dmg/download'
+  url 'https://sourceforge.net/projects/wesnoth/files/wesnoth/wesnoth-1.11.8/Wesnoth_1.11.8.dmg/download'
   homepage 'http://www.wesnoth.org/'
-  version '1.11.7'
-  sha1 '4620f60f6a60ecf598de2d83cc3060ae37c42c90'
+  version '1.11.8'
+  sha1 '4f6e89428727171c2ae50cc8183bfca9e87f0ae1'
   link 'Wesnoth.app'
 end
