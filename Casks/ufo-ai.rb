154e1b07b63cfc2a75d5ac4f92218b44643b2841
Bradley Rowe <bradley.rowe1@gmail.com>
2014-01-09 19:03:12 -0700

    added ufo-ai game

---
+++ Casks/ufo-ai.rb
@@ -0,0 +1,7 @@
+class UfoAi < Cask
+  url 'https://sourceforge.net/projects/ufoai/files/UFO_AI%202.x/2.4/ufoai-2.4-macosx-i386.dmg/download'
+  homepage 'http://ufoai.org/wiki/News'
+  version '2.4'
+  sha1 'c48c3dd5bb5802caf5712cf1cca94c10ccfd19a9'
+  link 'UfoAI.app'
+end
