a4e525e263849ad56b4eeb7601269f21b02386de
Bradley Rowe <bradley.rowe1@gmail.com>
2014-01-19 12:59:48 -0700

    added cask: eclipse dist with xtext and xtend

---
+++ Casks/eclipse-ide-dsl.rb
@@ -0,0 +1,7 @@
+class EclipseIdeDsl < Cask
+  url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/SR1/eclipse-dsl-kepler-SR1-macosx-cocoa-x86_64.tar.gz'
+  homepage 'http://eclipse.org'
+  version '4.3.1'
+  sha1 'f59ea648663c49431e5089879ef4a9ba508b5f7f'
+  link 'eclipse/Eclipse.app'
+end
