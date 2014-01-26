9c3c4e8e7772c3eb1c2309b30fed0464c4bf0430
Bradley Rowe <bradley.rowe1@gmail.com>
2014-01-26 15:23:20 -0700

    removed brackets.app, sprint 35

--- Casks/brackets.rb
+++
@@ -1,7 +0,0 @@
-class Brackets < Cask
-  url 'http://download.brackets.io/file.cfm?platform=OSX&build=35'
-  homepage 'http://brackets.io/'
-  version 'v35'
-  sha1 'b5387f9eb741da1d201b1c879e04a49d1d39f429'
-  link 'Brackets.app'
-end
