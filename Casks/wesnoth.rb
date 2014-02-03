c569ec2ccc525c70b9f99679ffe51c87bc48cae3
Bradley Rowe <bradley.rowe1@gmail.com>
2014-02-03 06:41:13 -0700

    remove wenoth cask, formula version is preferred

--- Casks/wesnoth.rb
+++
@@ -1,7 +0,0 @@
-class Wesnoth < Cask
-  url 'https://sourceforge.net/projects/wesnoth/files/wesnoth-1.10/wesnoth-1.10.7/Wesnoth_1.10.7.dmg/download'
-  homepage 'http://www.wesnoth.org/'
-  version '1.10.7'
-  sha1 '86489a4af7b243c0aa06788387f4b1c5db7cc240'
-  link 'Wesnoth.app'
-end
